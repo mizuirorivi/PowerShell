2439c22d5a5363bf9e069df2d023030dd68e71e7
mizuirorivi <onodaha@gmail.com>
2023-07-13 12:13:45 -0700

    set script directory ant then  we can use script in Script/

--- Scripts/my-profile.ps1
+++ Scripts/my-profile.ps1
@@ -1,4 +1,5 @@
 ﻿# MY POWERSHELL PROFILE (this profile file defines the look & feel of PowerShell)
+# set "$ScriptDirectory = (script directory path)" if run set-profile.ps1
 
 # WINDOW TITLE
 if ($IsLinux) { $Username = $(whoami) } else { $Username = $env:USERNAME }
@@ -6,10 +7,11 @@ $host.ui.RawUI.WindowTitle = "$Username @ $(hostname)"
 
 # COMMAND PROMPT
 function prompt { write-host -noNewline -foregroundColor yellow "`n➤"; return " " }
+# SET SCRIPT DIRECTORY 
 
 # ALIAS NAMES
 del alias:pwd -force -errorAction SilentlyContinue
-set-alias -name pwd -value list-workdir.ps1	# pwd = print working directory
+set-alias -name pwd -value $ScriptDirectory/$list-workdir.ps1	# pwd = print working directory
 set-alias -name ll -value get-childitem		# ll = list folder (long format)
 del alias:ls -force -errorAction SilentlyContinue 
-set-alias -name ls -value list-folder.ps1	# ls = list folder (short format)
+set-alias -name ls -value $ScriptDirectory/list-folder.ps1	# ls = list folder (short format)
--- Scripts/set-profile.ps1
+++ Scripts/set-profile.ps1
@@ -12,17 +12,33 @@
 #>
 
 try {
-	"⏳ (1/3) Querying path to PowerShell profile 'CurrentUserCurrentHost'..."
+	"⏳ (1/4) Querying path to PowerShell profile 'CurrentUserCurrentHost'..."
 	$PathToProfile = $PROFILE.CurrentUserCurrentHost
 	"$PathToProfile"
 
-	"⏳ (2/3) Creating the profile (if non-existent)..."
+	"⏳ (2/4) Creating the profile (if non-existent)..."
 	$Null = New-Item -Path $profile -ItemType "file" -Force
 
-	"⏳ (3/3) Copying my-profile.ps1..."
+	"⏳ (3/4) Copying my-profile.ps1..."
 	$PathToRepo = "$PSScriptRoot/.."
 	Copy-Item "$PathToRepo/Scripts/my-profile.ps1" "$PathToProfile" -force
-
+	"⏳ (4/4) Setting Script Directory to my-profile.ps1..."
+	$foundLine = $false
+	$newTextToAdd = '$ScriptDirectory = ' + "`"$PSScriptRoot`""
+	$tempFile = "temp.ps1"
+	Write-Output $PathToProfile
+	Get-Content $PathToProfile | ForEach-Object {
+		if($foundLine){
+			Add-Content -Path $tempFile -Value $newTextToAdd
+			$foundLine = $false
+		}
+		if($_ -match "# SET SCRIPT DIRECTORY "){
+			Write-Output "found line"
+			$foundLine = $true
+		}
+		Add-Content -Path $tempFile -Value $_
+	}
+	Move-Item -Path $tempFile -Destination $PathToProfile -Force
 	"✔️ updated your PowerShell profile by my-profile.ps1 - it gets active on next login"
 	exit 0 # success
 } catch {
