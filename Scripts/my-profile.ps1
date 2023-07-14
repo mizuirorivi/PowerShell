9e9e67c462938d9595d26463d513ee581dc2cbe8
mizuirorivi <onodaha@gmail.com>
2023-07-14 00:34:11 -0700

    add path for VC/bin

--- Scripts/my-profile.ps1
+++ Scripts/my-profile.ps1
@@ -12,13 +12,22 @@ function prompt { write-host -noNewline -foregroundColor yellow "`n➤"; return 
 # ALIAS NAMES
 
 del alias:pwd -force -errorAction SilentlyContinue
-set-alias -name pwd -value $ScriptDirectory/$list-workdir.ps1	# pwd = print working directory
+set-alias -name pwd -value "$ScriptDirectory/list-workdir.ps1"	# pwd = print working directory
 set-alias -name ll -value get-childitem		# ll = list folder (long format)
 del alias:ls -force -errorAction SilentlyContinue 
-set-alias -name ls -value $ScriptDirectory/list-folder.ps1	# ls = list folder (short format)
+set-alias -name ls -value "$ScriptDirectory/list-folder.ps1"	# ls = list folder (short format)
 # SET PATH
-$env:Path += "$ScriptDirectory"
-
+$upath =  [System.Environment]::GetEnvironmentVariable("Path","User")
+$syspath =  [System.Environment]::GetEnvironmentVariable("Path","Machine")
+$item_for_path = "$ScriptDirectory","C:\Users\rivi\Tools\SysinternalsSuite","C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin"
+foreach($item in  $item_for_path){
+  $upath += ";" + $item
+  $syspath += ";" + $item
+}
+[System.Environment]::SetEnvironmentVariable("Path", $upath, "Machine")
+[System.Environment]::SetEnvironmentVariable("Path", $syspath + ";" + $newPath, "User")
+$env:Path = $upath
+$env:SysPath = $syspath
 # ALIAS APPS
 set-alias -name typora -value "C:\Program Files\Typora\Typora.exe"
 set-alias -name marktext -value "C:\Users\rivi\AppData\Local\Programs\MarkText\MarkText.exe"
